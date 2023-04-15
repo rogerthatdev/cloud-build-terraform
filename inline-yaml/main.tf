b15f02a8941624ac0fa8c6ba1a9382342205d973
rogerthatdev <roger.that.dev@gmail.com>
2023-04-14 23:07:25 -0700

    example using yaml file

--- inline-yaml/main.tf
+++ inline-yaml/main.tf
@@ -1,17 +1,26 @@
-locals {
-  with_vars_config = yamldecode(templatefile("${path.root}/cloudbuild/with-variables.cloudbuild.yaml", { "variable_here" = "WORLD" }))
-  simple_config    = yamldecode(templatefile("${path.root}/cloudbuild/simple.cloudbuild.yaml", {}))
-}
-
+# This Cloud Source Repo is used as a quick placeholder to use for the trigger 
+# set up. 
 resource "google_sourcerepo_repository" "placeholder" {
   project = var.project_id
   name    = "placeholder-repo"
 }
+########################
+# The simplest example #
+########################
 
-resource "google_cloudbuild_trigger" "inline_github_with_vars" {
+/* 
+This first trigger will have the following build config configured inline:
+steps:
+  - name: ubuntu
+    args:
+      - echo
+      - hello world hey
+timeout: 600s
+*/
+resource "google_cloudbuild_trigger" "simplest_inline" {
   project     = var.project_id
   location    = var.region
-  name        = "trigger-with-inline-yaml-and-vars"
+  name        = "01-simplest-inline-config-example"
   description = "This trigger is deployed by terraform, with an in-line build config."
   trigger_template {
     branch_name  = "^main$"
@@ -23,14 +32,51 @@ resource "google_cloudbuild_trigger" "inline_github_with_vars" {
     images        = []
     substitutions = {}
     tags          = []
-    # This is the dynamic block that will create a step block for each step in
-    # the cloud build config file.
-    dynamic "step" {
-      for_each = local.with_vars_config.steps
-      content {
-        args = step.value.args
-        name = step.value.name
-      }
+    # This works because it's only one step
+    step {
+      name = "ubuntu"
+      args = [
+        "echo",
+        "hello world hey"
+      ]
+
+    }
+    timeout = "600s"
+  }
+}
+
+####################
+# A second example #
+####################
+/*
+This next trigger will get its yaml config from an actual file, passed along
+using Terraform's built-in yamldecode(). 
+
+Let's get that yaml in here using a locals block:
+*/
+locals {
+  simple_config_02 = yamldecode(templatefile("${path.root}/cloudbuild/02-simple.cloudbuild.yaml", {}))
+}
+
+resource "google_cloudbuild_trigger" "simple_inline" {
+  project     = var.project_id
+  location    = var.region
+  name        = "02-simple-inline-config-example"
+  description = "This trigger is deployed by terraform, with an in-line build config."
+  trigger_template {
+    branch_name  = "^main$"
+    invert_regex = false
+    project_id   = var.project_id
+    repo_name    = google_sourcerepo_repository.placeholder.name
+  }
+  build {
+    images        = []
+    substitutions = {}
+    tags          = []
+    # The single step is configured with name and args from the local value configured above
+    step {
+      name = local.simple_config_02.steps[0].name
+      args = local.simple_config_02.steps[0].args
     }
   }
 }
